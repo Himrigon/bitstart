a5a94510373398d0307d421e56536881cd4639d3
Himrigon <dimitriusanin@yandex.ru>
2018-02-04 22:54:22 +0300

    blocks

--- app/index.js
+++ app/index.js
@@ -1,8 +1,18 @@
 import './scss/app.scss'
 import video from './video/BSClub_low2.mp4'
+const PERCENT = 216
 
 $(function(){
   $('#video').append(`<source src="${video}" type="video/mp4">`)
+
+  function animateNumber(curNumber,int){
+    var start = 0
+    var interval = setInterval(()=>{
+      start < curNumber ? (start = start + 1) : clearInterval(interval)
+      $('#header-percent').html(start)
+    },int)
+  }
+  animateNumber(PERCENT, 5)
 })
 
 
