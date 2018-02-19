1a2a1ebcac5cc140fe419b83672d567351377dde
Himrigon <dimitriusanin@yandex.ru>
2018-02-19 13:36:38 +0300

    animation

--- app/index.js
+++ app/index.js
@@ -15,6 +15,8 @@ const scrollToElement = (element, duration) => {
     var percent =  Math.pow(time / duration, 2)
 
     window.scrollTo(0, startingY + diff * percent)
+    console.log(window.pageYOffset,targetY)
+    if(window.pageYOffset > targetY){return}
     if (time < duration) {
       window.requestAnimationFrame(step)
     }
@@ -42,27 +44,34 @@ const animateNumber = ( number, duration, elem) => {
 }
 
 $(function(){
-  $('#video').append(`<source src="${video}" type="video/mp4">`)
-  var finance = document.getElementById("finance")
-  $('.nav__list li').click(function(){scrollToElement($(this).data('target'), 700)})
-
-  $('#logo').click(function(){scrollToElement(0, 700)})
+  var financeWatcher = scrollMonitor.create( document.getElementById("finance"), -50 )
+  var infoBlockWather = scrollMonitor.create( document.getElementById("info-block"), -140 )
+  var infoBlock = $('.info-block')
+  var solutions = $('.solutions')
 
   animateNumber(PERCENT, 1000 ,'header-percent')
 
+  $('#video').append(`<source src="${video}" type="video/mp4">`)
+
+  $('.nav__list li').click(function(){scrollToElement($(this).data('target'), 700)})
+  $('#logo').click(function(){scrollToElement(0, 700)})
 
 
 
-var elementWatcher = scrollMonitor.create( finance );
 
-elementWatcher.enterViewport(function() {
-    animateNumber(50, 1500, 'invest')
-    animateNumber(250, 1500, 'buisnes')
-    animateNumber(1000, 1500, 'premium')
-});
-elementWatcher.exitViewport(function() {
-    console.log( 'I have left the viewport' );
-});
+
+
+  financeWatcher.enterViewport(function() {
+      animateNumber(50, 1500, 'invest')
+      animateNumber(250, 1500, 'buisnes')
+      animateNumber(1000, 1500, 'premium')
+      solutions.addClass('solutions--animated')
+
+  });
+  infoBlockWather.enterViewport(function() {
+      infoBlock.addClass('info-block--animated')
+  });
+
 })
 
 
